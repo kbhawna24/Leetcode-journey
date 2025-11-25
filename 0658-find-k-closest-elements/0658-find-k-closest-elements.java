18183b8e19e89f45817e23d735fc80c77adfb0dc
Bhawna Kumari <kbhawna2409@gmail.com>
2025-11-24 22:05:47 -0500

    Time: 7 ms (46.3%), Space: 47.8 MB (23.19%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,22 +1,49 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         
-        List<Integer> list = new ArrayList<>();
-        for(int i: arr)
-        list.add(i);
-        Collections.sort(list, (a,b) -> {
-            int distA = Math.abs(a - x);
-            int distB = Math.abs(b - x);
-
-            if (distA == distB) {
-                return a - b;
-            }
-            return distA - distB;
-
-        });
+        List<Integer> output = new ArrayList<>();
+        List<Integer> input = new ArrayList<>();
+        for(int l: arr)
+        input.add(l);
+        int start_idx = binarySearch(arr,x);
+        System.out.println(start_idx);
         
-        List<Integer> result = new ArrayList<>(list.subList(0, k));
-        Collections.sort(result);
-        return result;
+        int i = start_idx - 1;
+        int j = start_idx;
+        
+        
+        while(k>0){
+            if(i<0)
+            j++;
+            else if(j>=arr.length)
+            i--;
+            
+            else if(Math.abs(arr[i] - x) <= Math.abs(arr[j]-x) )
+                i--;
+            else
+            j++;
+
+            k--;
+            
+        }
+    
+        output = input.subList(i+1,j);
+
+        return output;
+        
+    }
+    public int binarySearch(int[] arr, int x){
+        int l = 0;
+        int r = arr.length - 1;
+        while(l<=r){
+            int m = (l+r)/2;
+            if (arr[m] == x)
+            return m;
+            else if(arr[m] > x)
+            r = m-1;
+            else
+            l = m + 1;
+        }
+        return l;
     }
 }
