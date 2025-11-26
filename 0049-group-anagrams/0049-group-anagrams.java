fe8283123fa22c386bb96e8d6c38f32ae8a3a8a0
Bhawna Kumari <kbhawna2409@gmail.com>
2025-11-26 12:08:10 -0500

    Time: 6 ms (98.59%), Space: 49.8 MB (18.67%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0)
+        return new ArrayList<>();
+        
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s: strs){
+            char[] array = s.toCharArray();
+            Arrays.sort(array);
+
+            String key = new String(array);
+            if(!map.containsKey(key))
+            map.put(key, new ArrayList<>());
+
+            map.get(key).add(s);
+
+        }
+        return new ArrayList<>(map.values());
+    }
+}
