1c061d592ee0f92bf07ae032488d344781e1f683
Bhawna Kumari <kbhawna2409@gmail.com>
2025-12-11 16:51:36 -0500

    Time: 34 ms (46.77%), Space: 59.3 MB (27.4%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,26 +2,22 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
         Arrays.sort(nums);
-        List<List<Integer>> twosum = new ArrayList<>();
+        int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
     
-      for(int i=0; i<nums.length-2; i++){
-        int target = 0 - nums[i];
+      for(int i=0; i<n-2; i++){
+
         if(i>0 && nums[i] == nums[i-1])
         continue;
-        twosum = twoSum(nums, i, target);
-        ans.addAll(twosum);
+
+        int left = i + 1;
+        int right = n - 1;
+
+        int target = 0 - nums[i];
         
-    }
-    return ans; 
-    }
-    public List<List<Integer>> twoSum(int[] nums, int fixed, int target){
-        int left = fixed + 1;
-        int right = nums.length - 1;
-        List<List<Integer>> ans = new ArrayList<>();
         while(left<right){
             if(nums[left] + nums[right] == target){
-            ans.add(Arrays.asList(nums[fixed] , nums[left], nums[right]));
+            ans.add(Arrays.asList(nums[i] , nums[left], nums[right]));
             left++;
             right--;
             while(left< right && nums[left] == nums[left-1])
@@ -35,6 +31,10 @@ class Solution {
             right--;
 
         }
-        return ans;
-    } 
+        
+        
+    }
+    return ans; 
+    }
+    
 }
